1122c8b540e0915149fd509c168ea31f37e7c20e
lephuongtu271094 <lephuongtu271094@gmail.com>
2017-01-27 01:14:18 +0700

    1

--- project/java.js
+++ project/java.js
@@ -1,47 +1,47 @@
+$(function () {
+    $('ul.nav li.dropdown').hover(function () {
+        $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(200);
+    }, function () {
+        $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(200);
+    });
+    var $animation_elements = $('.animation-element');
+    var $window = $(window);
 
-$(function(){
-	$('ul.nav li.dropdown').hover(function() {
-    $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(200);
-  }, function() {
-    $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(200);
-});
-var $animation_elements = $('.animation-element');
-var $window = $(window);
+    function check_if_in_view() {
+        var window_height = $window.height();
+        var window_top_position = $window.scrollTop();
+        var window_bottom_position = (window_top_position + window_height);
 
-function check_if_in_view() {
-  var window_height = $window.height();
-  var window_top_position = $window.scrollTop();
-  var window_bottom_position = (window_top_position + window_height);
- 
-  $.each($animation_elements, function() {
-    var $element = $(this);
-    var element_height = $element.outerHeight();
-    var element_top_position = $element.offset().top;
-    var element_bottom_position = (element_top_position + element_height);
- 
-    //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
-        (element_top_position <= window_bottom_position)) {
-      $element.addClass('in-view');
-    } else {
-      $element.removeClass('in-view');
+        $.each($animation_elements, function () {
+            var $element = $(this);
+            var element_height = $element.outerHeight();
+            var element_top_position = $element.offset().top;
+            var element_bottom_position = (element_top_position + element_height);
+
+            //check to see if this current container is within viewport
+            if ((element_bottom_position >= window_top_position) &&
+                (element_top_position <= window_bottom_position)) {
+                $element.addClass('in-view');
+            } else {
+                $element.removeClass('in-view');
+            }
+        });
     }
-  });
-}
 
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
+    $window.on('scroll resize', check_if_in_view);
+    $window.trigger('scroll');
 
-$('.bttop').on('click' , function(){
-	$('html,body').animate({
-		scrollTop:0
-	},2000);return false;
-});
-$(window).scroll(function(){
-	if($(this).scrollTop() > 500){
-		$('.bttop').fadeIn();
-	}else {
-		$('.bttop').fadeOut();
-	}
-});
+    $('.bttop').on('click', function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 2000);
+        return false;
+    });
+    $window.scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('.bttop').fadeIn();
+        } else {
+            $('.bttop').fadeOut();
+        }
+    });
 })
